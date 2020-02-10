9a6eee13abb171e14b712ec5c66168bf57de720c
Dan <dmoldova@usc.edu>
2020-02-09 22:27:09 -0800

    coded microphone subprocess

--- cam_test.py
+++ cam_test.py
@@ -13,6 +13,9 @@ echo = 5
 trig = 6
 Relay_Ch1 = 20
 
+global dur
+dur= 10
+
 GPIO.setup(echo, GPIO.IN)
 GPIO.setup(trig, GPIO.OUT)
 GPIO.setup(Relay_Ch1, GPIO.OUT)
@@ -23,8 +26,8 @@ def cam():
     tim = str(tim)
     GPIO.output(Relay_Ch1, GPIO.HIGH)
     try:
-        camera.start_recording('/home/pi/Project/vids/test/' + tim + '.h264')
-        time.sleep(10)
+        camera.start_recording('/home/pi/Recycling-ML-Project/vids/test/' + tim + '.h264')
+        time.sleep(dur)
         camera.stop_recording()
         GPIO.output(Relay_Ch1, GPIO.LOW)
     except:
@@ -35,9 +38,17 @@ def print_accel():
     tim = str(tim)
     prim_tim = datetime.datetime.now().second
     fin_tim = datetime.datetime.now().second
-    while fin_tim - prim_tim < 10:
-        print("%f %f %f" %acc.acceleration, file = open("/home/pi/Project/accel/test/" + tim +".txt", "a"))
+    while fin_tim - prim_tim < dur:
+        print("%f %f %f" %acc.acceleration, file = open("/home/pi/Recycling-ML-Project/accel/test/" + tim +".txt", "a"))
         fin_tim = datetime.datetime.now().second
+
+def mic():
+    tim = datetime.datetime.now()
+    tim = str(tim)
+    name = tim + '.wav'
+    print(name)
+    cmd = f"arecord -D plughw:1 -c1 -r 48000 -f S32_LE -t wav --duration={dur} -V mono -v {name}"
+    subprocess.Popen(cmd, shell=True)
 
 def main():
     while True:
@@ -64,9 +75,13 @@ def main():
 
             thread2 = threading.Thread(target = print_accel, args=())
             thread2.start()
+
+            thread3 = threading.Thread(target = mic, args=())
+            thread3.start()
             
             thread1.join()
             thread2.join()
+            thread3.join()
 
 
 if __name__ == "__main__":
